dd9f622d8686b7a0da3c0633e73006847f7483d2
mariam abulail <mariamabulail88@gmail.com>
2024-10-22 12:14:40 +0300

    last edit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import "./App.css";
 
 function App() {
   const persons = [
-    { name: "Carolien Bloeme", isChecked: false },
-    { name: "Sun Jun", isChecked: true },
-    { name: "Song Bao", isChecked: false },
-    { name: "Olivia Arribas", isChecked: true },
-    { name: "BonginkosiMdladlana", isChecked: false },
-    { name: "Arina Belomestnykh", isChecked: true },
-    { name: "Jacqueline Likoki", isChecked: true },
+    { name: "Carolien Bloeme", isChecked: false,id:1 },
+    { name: "Sun Jun", isChecked: true,id:2 },
+    { name: "Song Bao", isChecked: false,id:3 },
+    { name: "Olivia Arribas", isChecked: true,id:4 },
+    { name: "BonginkosiMdladlana", isChecked: false,id:5 },
+    { name: "Arina Belomestnykh", isChecked: true ,id:6},
+    { name: "Jacqueline Likoki", isChecked: true,id:7 },
   ];
   return (
     <div className="main-contianer App">
       <h2>Add members to Front-end development team</h2>
       <FindMember />
       {
-        persons.map((person)=>(<Person name={person.name} isChecked={person.isChecked}/>))
-      }{/* with help*/}
+        persons.map((person)=>(<Person  key={person.id}name={person.name} isChecked={person.isChecked}/>))
+      }{/* with*/}
       {/* 
       <Person name="Carolien Bloeme" isChecked={false} />
       <Person name="Sun Jun" isChecked={true} />
